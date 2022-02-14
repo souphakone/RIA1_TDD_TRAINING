36e6bb390d2f263d8864be1eb9bdd188d1bdf5bc
SAMOUTPHONH Souphakone <Souphakone.SAMOUTPHONH@cpnv.ch>
2022-02-14 12:06:07 +0100

    fix test

--- src/Cart.js
+++ src/Cart.js
@@ -8,38 +8,39 @@
 
 "use strict";
 
-module.exports = class Cart{
+module.exports = class Cart {
+  //region private attributes
+  items = null;
+  //endregion private attributes
 
-    //region private attributes
-    items = null;
-    //endregion private attributes
+  /**
+   * @brief This method constructs a Cart Object
+   * @param items : list of cartItems
+   */
+  constructor(items = null) {
+    this.items += items;
+  }
 
+  /**
+   * @brief This property returns the list of CartItems presents in the Cart.
+   * @exception EmptyCartException is thrown if the Cart is empty
+   */
+  get items() {
+    throw new Error("Method not implemented.");
+  }
 
-    /**
-     * @brief This method constructs a Cart Object
-     * @param items : list of cartItems
-     */
-    constructor(items = null){
-        throw new Error('Method not implemented.');
+  /**
+   * @brief This property returns the total of the Cart.
+   * @exception EmptyCartException is thrown if the Cart is empty
+   */
+  get totalPrice() {
+    if (this.items != 0) {
+    } else {
+      return 0;
     }
+  }
+  //endregion public methods
 
-    /**
-     * @brief This property returns the list of CartItems presents in the Cart.
-     * @exception EmptyCartException is thrown if the Cart is empty
-     */
-    get items(){
-        throw new Error('Method not implemented.');
-    }
-
-    /**
-     * @brief This property returns the total of the Cart.
-     * @exception EmptyCartException is thrown if the Cart is empty
-     */
-    get totalPrice(){
-        throw new Error('Method not implemented.');
-    }
-    //endregion public methods
-
-    //region private methods
-    //endregion private methods
-}
+  //region private methods
+  //endregion private methods
+};
